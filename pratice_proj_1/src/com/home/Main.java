fb03a494a02fd660ceb5cf65f6909c3fa0db6532
snigdhesh <snigdhesh@live.com>
2020-11-11 22:38:36 -0500

    Added comments

--- pratice_proj_1/src/com/home/Main.java
+++ pratice_proj_1/src/com/home/Main.java
@@ -2,41 +2,40 @@ package com.home;
 
 import com.home.models.Array;
 
-import java.util.Arrays;
-
 public class Main {
 
-    public static void main(String[] args) throws IllegalAccessException {
-        Array array = new Array(4);
-        array.insert(152);
-        array.insert(252);
-        array.insert(352);
-        array.insert(452);
-        array.print();
-        array.insertAt(5,3);
-        System.out.println("Inserted 5 at position 3");
-        array.print(); //1235
-        array.insertAt(5,2);
-        System.out.println("Inserted 5 at position 2");
-        array.print();
-    }
+  public static void main(String[] args) throws IllegalAccessException {
+    Array array = new Array(4);
+    array.insert(152);
+    array.insert(252);
+    array.insert(352);
+    array.insert(452);
+    array.print();
+    array.insertAt(5, 3);
+    System.out.println("Inserted 5 at position 3");
+    array.print(); //1235
+    array.insertAt(5, 2);
+    System.out.println("Inserted 5 at position 2");
+    array.print();
+  }
 
-
-    public static int getFactorial(int num) {
-        int res = 1;
-        while (num != 1) {
-            res = res * num--;
-        }
-        return res;
+  //Method1: Get factorial of a given number
+  public static int getFactorial(int num) {
+    int res = 1;
+    while (num != 1) {
+      res = res * num--;
     }
+    return res;
+  }
 
-    public static int getFactorial2(int num) {
-        int res = 1;
-        for (int i = 1; i <= num; i++) {
-            res = res * i;
-        }
-        return res;
+  //Method2: Get factorial of a given number
+  public static int getFactorial2(int num) {
+    int res = 1;
+    for (int i = 1; i <= num; i++) {
+      res = res * i;
     }
+    return res;
+  }
 
 }
 
--- pratice_proj_1/src/com/home/models/Array.java
+++ pratice_proj_1/src/com/home/models/Array.java
@@ -4,86 +4,89 @@ import java.util.Arrays;
 
 public class Array {
 
-    private int[] items;
-    int count = 0;
+  private int[] items;
 
-    public Array(int arrayLength) {
-        this.items = new int[arrayLength];
+  int count = 0;
+
+  public Array(int arrayLength) {
+    this.items = new int[arrayLength];
+  }
+
+  public void insert(int item) {
+    if (count == this.items.length) {
+      int[] newItems = new int[count * 2];
+      for (int i = 0; i < items.length; i++) {
+        newItems[i] = items[i];
+      }
+      items = newItems;
+    }
+    items[count++] = item;
+  }
+
+  public void removeAt(int index) throws IllegalAccessException {
+    if (index < 0 || index >= count) {
+      throw new IllegalAccessException("Invalid index");
+    }
+    for (int i = index; i < items.length - 1; i++) {
+      items[i] = items[i + 1];
+    }
+    count--;
+  }
+
+  public void insertAt(int item, int index) throws IllegalAccessException {
+    if (index < 0 || index >= count) {
+      throw new IllegalAccessException("Invalid index. Index can be between 0 and " + count);
     }
 
-    public void insert(int item) {
-        if (count == this.items.length) {
-            int[] newItems = new int[count * 2];
-            for (int i = 0; i < items.length; i++) {
-                newItems[i] = items[i];
-            }
-            items = newItems;
-        }
-        items[count++] = item;
+    if (count < items.length) {
+      int[] newItems = new int[items.length];
+      for (int i = 0; i < items.length; i++) {
+        newItems[i] = items[i];
+      }
+      for (int i = index; i < count; i++) {
+        items[i + 1] = newItems[i];
+      }
+      items[index] = item;
     }
-
-    public void removeAt(int index) throws IllegalAccessException {
-        if (index < 0 || index >= count) {
-            throw new IllegalAccessException("Invalid index");
-        }
-        for (int i = index; i < items.length - 1; i++) {
-            items[i] = items[i + 1];
-        }
-        count--;
-    }
-
-    public void insertAt(int item, int index) throws IllegalAccessException {
-        if (index < 0 || index >= count) {
-            throw new IllegalAccessException("Invalid index. Index can be between 0 and " + count);
-        }
-        //increase array length
-        //if number of elements in an array is equal to array length ex: count = 5 array lenth =5
-        if (count < items.length) {
-            int[] newItems = new int[items.length];
-            for (int i = 0; i < items.length; i++) {
-                newItems[i] = items[i];
-            }
-            for (int i = index; i < count; i++) {
-                items[i + 1] = newItems[i];
-            }
-            items[index] = item;
-        } else {
+        /*We are throwing an exception in case when index is out of range, so exception is thrown even when array is full.
+        But if we want to test insertAt() method, when array is full, following code helps.
+        else {
             int[] newItems = new int[count * 2];
             for (int i = 0; i < count; i++) {
                 newItems[i] = items[i];
             }
             items = newItems;
             insertAt(item, index);
-        }
+        }*/
 
-    }
+  }
 
-    public int indexOf(int item) {
-        for (int i = 0; i < items.length; i++) {
-            if (item == this.items[i]) {
-                return i;
-            }
-        }
-        return -1;
+  public int indexOf(int item) {
+    for (int i = 0; i < items.length; i++) {
+      if (item == this.items[i]) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    public int max() {
-        Arrays.sort(items);
-        return items[items.length - 1];
-    }
+  public int max() {
+    Arrays.sort(items);
+    return items[items.length - 1];
+  }
 
-    public void reverse() {
-        int[] newItems = new int[count];
-        int index = count - 1;
-        for (int i = 0; i < count; i++) {
-            newItems[i] = items[index--];
-        }
-        items = newItems;
+  public void reverse() {
+    int[] newItems = new int[count];
+    int index = count - 1;
+    for (int i = 0; i < count; i++) {
+      newItems[i] = items[index--];
     }
+    items = newItems;
+  }
 
-    public void print() {
-        for (int i = 0; i < count; i++) {
-            System.out.println(items[i]);
-        }
+  public void print() {
+    for (int i = 0; i < count; i++) {
+      System.out.println(items[i]);
     }
+  }
 }
